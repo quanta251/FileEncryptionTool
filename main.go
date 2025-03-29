7a9925b1360335bcfd13aa407e0a96e17f1d74cb
quanta251 <haydenbritt25@gmail.com>
2025-03-29 15:25:50 -0500

    Followed the go documentation on how to create an AES-CTR Stream Cipher. Still studying to see how this really works. Where to pick up? Why is aes.BlockSize used and not the size of the key?

--- main.go
+++ main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
-  "fmt"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main() {
-  filename := "./myfile.txt"
+func getData(path string) []byte {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal("Could not open the file at:", path)
+	}
 
-  crypto.Decrypter
+	return data
+}
+
+func main() {
+	const key_size = 32
+
+	// Get the plaintext from the stored file.
+	plaintext := getData("./myfile.txt")
+
+	// Generate the key for this encryption
+	key := make([]byte, key_size)
+	check, err := rand.Read(key)
+	if err != nil || check != key_size {
+		log.Fatal("Something went wrong:", err)
+	}
+
+	// Create the cipher block that will be used to encrypt the plaintext
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	CipherBlock, err := aes.NewCipher(key)
+
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCTR(CipherBlock, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	fmt.Println("The new ciphertext is:", string(ciphertext))
+
 }
