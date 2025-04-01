b42b3356a73b32034b5e4e3b645f39f5faa19ccc
quanta251 <haydenbritt25@gmail.com>
2025-03-31 21:51:48 -0500

    The encryption and subsequent decryption is working now. I just need to implement way to read in larger files instead of just small files.

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
+ 
+const aes_KEYSIZE = 32
 
 func getData(path string) []byte {
 	data, err := os.ReadFile(path)
@@ -25,32 +26,62 @@ func main() {
 	fmt.Println("plaintext:", string(plaintext))
 
 	// Establish a key to use
-	key := make([]byte, 32)
-	io.ReadFull(rand.Reader, key)
-	fmt.Println("The key is now:", key)
+  key := make([]byte, aes_KEYSIZE)
+  _, err := rand.Read(key)
+  if err != nil {
+    log.Fatal("Could not generate a key:", err)
+  }
 
+  // Make the cipher block that will be used
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatal("Could not create aes.NewCipher object:", err)
 	}
 
+  // Make a buffer to store the ciphertext
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+  // Generate the Initialization Vector (IV)
+  iv := make([]byte, aes.BlockSize)
+	// if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+  if _, err := rand.Read(iv); err != nil {
 		log.Fatal("Could generate the IV:", err)
 	}
 
+  // Store the IV in the first part of the ciphertext
+  copy(ciphertext, iv)
+  fmt.Println("The ciphertext buffer just after copying the IV is:", ciphertext)
+
 	// Create the Stream Cipher.
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertext, plaintext)
-	fmt.Println("ciphertext:", string(ciphertext))
+  stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+	fmt.Println("ciphertext after encryption:", string(ciphertext))
 
-	plaintext2 := make([]byte, len(ciphertext))
-	stream = cipher.NewCTR(block, iv)
-	stream.XORKeyStream(plaintext2, ciphertext)
+  //-------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------
 
-	fmt.Println("The ciphertext generated is:", string(ciphertext))
-	fmt.Println("The plaintext generated is:", string(plaintext2))
+	plaintext2 := make([]byte, len(ciphertext) - aes.BlockSize)
+  ciphertext2 := make([]byte, len(ciphertext))
+  iv2 := make([]byte, aes.BlockSize)
 
+  // copy the ciphertext into the new buffer
+  copy(ciphertext2, ciphertext)
+
+  // Read the IV from the first part of the ciphertext
+  iv2 = ciphertext2[:aes.BlockSize]
+
+  block2, err := aes.NewCipher(key)
+  if err != nil {
+    log.Fatal("Could not create the second cipher block:", err)
+  }
+
+  stream2 := cipher.NewCTR(block2, iv2)
+
+  stream2.XORKeyStream(plaintext2, ciphertext2[aes.BlockSize:])
+
+  fmt.Println("The decrypted plaintext is:", string(plaintext2), "\n\n\n")
+  fmt.Println("----------------------------------")
+  fmt.Println("Program finished")
+  fmt.Println("----------------------------------")
 }
